f901d58b9016cabb7db06cf6a2e1416a6a79a4ac
Diego Dimitroff Petcoff <82569859+DiegoDimitroffPetcoff@users.noreply.github.com>
2023-03-11 19:21:41 +0000

    Javascript easy practice

---
+++ .github/index.js
@@ -0,0 +1,10 @@
+const name = "Diego";
+const edad = 33;
+const developer = true;
+const date = new Date(" 31 july 1989");
+console.log(date) 
+const favoriteBook = {
+    titulo: "El principito",
+    autor: "Saint it saperi",
+    fecha: 1989,
+}
