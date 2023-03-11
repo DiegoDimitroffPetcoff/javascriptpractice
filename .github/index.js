be9fc19e14a40bfa57e9e1b208e6bad6e459dd3e
Diego Dimitroff Petcoff <82569859+DiegoDimitroffPetcoff@users.noreply.github.com>
2023-03-11 23:19:01 +0000

    Factoria de 10 en buCle For, If y while

--- .github/index.js
+++ .github/index.js
@@ -2,9 +2,37 @@ const name = "Diego";
 const edad = 33;
 const developer = true;
 const date = new Date(" 31 july 1989");
-console.log(date) 
+
 const favoriteBook = {
-    titulo: "El principito",
-    autor: "Saint it saperi",
-    fecha: 1989,
+  titulo: "El principito",
+  autor: "Saint it saperi",
+  fecha: 1989,
+};
+
+/*FACTORIAL DE 10 CON EL BUCLE FOR
+let inicio = 1;
+for (i = 1; i <= 10; i++) {
+  inicio = inicio * i;
+
+//FACTORIAL DE 10 CON EL BUCLE WHILE
+let i = 1;
+let numero = 1;
+while (i <= 10) {
+  numero = numero * i;
+  i++;
+}
+console.log(numero)
+}*/
+
+
+//FACTORIAL DE 10 CON EL BUCLE WHILE + BIFURCACION + BREAK
+let inicio = 1;
+let i = 1;
+while (inicio) {
+  inicio = inicio * i;
+  i++;
+
+  if (i > 10) {
+    break;
+  }
 }
