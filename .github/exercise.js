8b6ed646b37d0257ca7df77508ad75045cdaf41c
Diego Dimitroff Petcoff <82569859+DiegoDimitroffPetcoff@users.noreply.github.com>
2023-03-12 19:04:09 +0000

    Excercise 2

---
+++ .github/exercise.js
@@ -0,0 +1,19 @@
+const nombre = "  Diego ";
+const apellido = "Dimitroff";
+const estudiante = nombre + " " + apellido;
+
+
+const estudianteMayus= estudiante.toUpperCase();
+const estudianteMinus= estudiante.toLowerCase();
+
+var numLetrasEstudiante = estudiante.length;
+
+var primeraLetra = estudiante[0]
+
+var primeraLetraNombre = estudiante[0]
+var ultimaLetraApellido = apellido.split('')[apellido.length - 1]
+
+var eliminaEspacios = estudiante.trim()
+
+var searchName = estudiante.includes("Diego")
+
