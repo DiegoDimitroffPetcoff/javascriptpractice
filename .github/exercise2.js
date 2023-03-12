6956a3e0695f576a2a0098b0949b700855b11c20
Diego Dimitroff Petcoff <82569859+DiegoDimitroffPetcoff@users.noreply.github.com>
2023-03-12 20:23:53 +0000

    Bumber ex

---
+++ .github/exercise2.js
@@ -0,0 +1,15 @@
+
+
+const alturaCentrimetros = 170
+const alturaMetros = 1.70
+
+const pesoFloat = 1.69
+
+const alturaMtsRedondeadaArriba = parseInt(alturaMetros)
+
+
+const maxpesoRedondeadoAbajo = Math.round(alturaMetros)
+
+
+var max = Number.MAX_VALUE + " es igual al maximo valor que se puede obtener en javascript"
+console.log(max)
